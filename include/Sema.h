6d6f4769c9a5d472975504fb62ca1630ab44b2fe
bomkei <93323570+bomkei@users.noreply.github.com>
2022-11-22 09:32:01 +0900

    [Sema] create Sema source files and sema-doc.md

---
+++ include/Sema.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "types/Token.h"
+#include "types/Object.h"
+
+class Sema {
+ public:
+  Sema(Sema&&) = delete;
+  Sema(Sema const&) = delete;
+
+  private:
+};
---
+++ src/Sema/Sema.cc
@@ -0,0 +1,16 @@
+#include <cassert>
+
+#include "types/Node.h"
+#include "types/BuiltinFunc.h"
+#include "Error.h"
+#include "Utils.h"
+#include "Sema.h"
+
+/*
+
+
+solve_variable_type(VariableContext&
+
+
+
+*/
